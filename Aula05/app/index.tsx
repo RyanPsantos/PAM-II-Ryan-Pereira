8e4927678ae4ded50efc44012196400a446a9173
Ryan Pereira <Ryanpsantos124@gmail.com>
2024-09-30 21:16:20 -0300

    Expo da aula cinco

--- Aula05/app/index.tsx
+++ Aula05/app/index.tsx
@@ -27,7 +27,36 @@ const App = () => {
       await createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
           // O usuário foi criado com sucesso
+          const user = userCredential.user;
+          console.log('Usuário criado com sucesso:', user);
         })
+        .catch((error) => {
+          //Tratar erros de cadastro
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.error('Erro ao criar usuário:', error);
+        });
+    } catch (error) {
+      console.error(error);
     }
-  }
-}
+  };
+
+  return (
+    <View>
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+      placeholder="Senha"
+      secureTextEntry
+      value={password}
+      onChangeText={setPassword}
+      />
+      <Button title="Cadastrar" onPress={handleSignup} />
+    </View>
+  );
+};
+
+export default App;
